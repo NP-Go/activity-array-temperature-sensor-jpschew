5a3bf2473608a4e48313b874db41957afa3e846b
jpschew <jps.chew@gmail.com>
2022-03-30 23:14:30 +0800

    added code to calculate average of array

--- temperatureSensor.go
+++ temperatureSensor.go
@@ -1,6 +1,18 @@
 package main
 
+import "fmt"
+
 func main() {
-	//temperature := [24]float64{20.1, 24, 27.3, 30.1, 26.4, 22.2, 20.1, 24, 27.3, 30.1, 26.4, 20.1, 24, 27.3, 30.1, 26.4, 20.1, 24, 27.3, 30.1, 26.4, 20.1, 24, 27.3}
+	temperature := [24]float64{20.1, 24, 27.3, 30.1, 26.4, 22.2, 20.1, 24, 27.3, 30.1, 26.4, 20.1, 24, 27.3, 30.1, 26.4, 20.1, 24, 27.3, 30.1, 26.4, 20.1, 24, 27.3}
 	//Insert Code here
+	fmt.Println(len(temperature))
+	var total, average float64
+
+	for i := 0; i < len(temperature); i++ {
+		total += temperature[i]
+	}
+	average = total / float64(len(temperature))
+
+	fmt.Printf("%.2f %.2f\n", total, average)
+
 }
